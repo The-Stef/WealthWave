d925c2bfd2fa0cd3481ee96a5ebe328c2f65ca7c
The-Stef <sdusnoki@constructor.university>
2024-06-28 14:14:39 +0200

    Added container for future components of the login page

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -7,10 +7,27 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         decoration: const BoxDecoration(
-      image: DecorationImage(
-        image: AssetImage('wealthwave_background.png'),
-        fit: BoxFit.fill,
-      ),
-    ));
+          image: DecorationImage(
+            image: AssetImage('wealthwave_background.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: const Center(
+          child: ColoredBlock(),
+        ));
+  }
+}
+
+class ColoredBlock extends StatelessWidget {
+  const ColoredBlock({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 896,
+      height: 960,
+      child: DecoratedBox(
+          decoration: BoxDecoration(color: Color.fromARGB(255, 7, 57, 60))),
+    );
   }
 }
