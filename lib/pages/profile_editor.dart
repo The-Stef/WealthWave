2d41eb213a5fa81d3a0844183c60cdcca9026d3d
The-Stef <sdusnoki@constructor.university>
2024-07-25 13:39:18 +0200

    Added all text fields to page

--- lib/pages/profile_editor.dart
+++ lib/pages/profile_editor.dart
@@ -1,6 +1,9 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromARGB(255, 25, 25, 25);
+const textColor = Color.fromARGB(255, 85, 214, 190);
 
 void main() {
   runApp(ProfileEditor());
@@ -25,10 +28,47 @@ class EditProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit Profile'),
+        backgroundColor: backgroundColor,
+        title: Text(
+          'Edit Profile',
+          style: TextStyle(
+            color: textColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: Center(
-        child: Text('WIP'),
+      body: Container(
+        color: backgroundColor,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(labelText: 'First Name'),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Last Name'),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Address'),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Phone Number'),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Country'),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'City'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
