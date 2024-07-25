dba81769e185ddabc71f45cfc039fca611825cc7
The-Stef <sdusnoki@constructor.university>
2024-07-25 13:45:18 +0200

    Modified text input to be InputLabelBlocks

--- lib/pages/profile_editor.dart
+++ lib/pages/profile_editor.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:wealthwave/pages/login_page.dart';
 
 const backgroundColor = Color.fromARGB(255, 25, 25, 25);
 const textColor = Color.fromARGB(255, 85, 214, 190);
@@ -33,6 +34,7 @@ class EditProfilePage extends StatelessWidget {
           'Edit Profile',
           style: TextStyle(
             color: textColor,
+            fontSize: 32,
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -44,26 +46,40 @@ class EditProfilePage extends StatelessWidget {
           child: Center(
             child: Column(
               children: [
-                TextField(
-                  decoration: InputDecoration(labelText: 'First Name'),
+                // Todo: Get user information to display here from db
+                InputLabelBlock(
+                  labelText: 'First Name',
+                  bodyText: 'Type your first name...',
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Last Name'),
+                SizedBox(height: 20),
+                InputLabelBlock(
+                  labelText: 'Last Name',
+                  bodyText: 'Type your last name...',
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Email'),
+                SizedBox(height: 20),
+                InputLabelBlock(
+                  labelText: 'Email',
+                  bodyText: 'Type your email...',
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Address'),
+                SizedBox(height: 20),
+                InputLabelBlock(
+                  labelText: 'Address',
+                  bodyText: 'Type your address...',
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Phone Number'),
+                SizedBox(height: 20),
+                InputLabelBlock(
+                  labelText: 'Phone Number',
+                  bodyText: 'Type your phone number...',
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Country'),
+                SizedBox(height: 20),
+                InputLabelBlock(
+                  labelText: 'Country',
+                  bodyText: 'Type your country...',
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'City'),
+                SizedBox(height: 20),
+                InputLabelBlock(
+                  labelText: 'City',
+                  bodyText: 'Type your city...',
                 ),
               ],
             ),
